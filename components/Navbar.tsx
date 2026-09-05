b536463e8988c0e3b0df876ac04f045f8905adbe
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-05 12:05:50 +0500

    coding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,63 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faPlus, faBell, faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faPlus, faBell, faCircleUser, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
         <>
-            <div className="flex items-center justify-between p-4 relative h-14 ">
+            <header className="flex h-14 items-center justify-between px-4">
+
+                {/* LEFT */}
+                <div className="flex items-center gap-4">
+                    <button className="flex h-10 w-10 items-center justify-center rounded-full hover:bg-gray-900 ">
+                        <FontAwesomeIcon icon={faBars} className="text-xl" />
+                    </button>
+
+                    <img
+                        src="/images/youtubelogo.png"
+                        alt="YouTube Logo"
+                        className="h-6 w-auto"
+                    />
+                </div>
+
+                {/* CENTER SEARCH */}
+                <div className="hidden flex-1 justify-center px-8 md:flex">
+                    <div className="relative w-full max-w-[600px]">
+
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            className="h-10 w-[600px] rounded-full border border-gray-300 bg-black pl-5 pr-14 outline-none focus:border-blue-500"
+                        />
+
+                        <button className="absolute right-0 top-0 flex h-10 w-14 items-center justify-center rounded-r-full bg-gray-900 hover:bg-blue-600">
+                            <FontAwesomeIcon
+                                icon={faMagnifyingGlass}
+                                className="text-lg text-white"
+                            />
+                        </button>
+
+                    </div>
+                </div>
+
+                {/* RIGHT */}
                 <div className="flex items-center gap-2">
-                    <FontAwesomeIcon icon={faBars} />
-                    <img src="/images/youtubelogo.png" alt="YouTube Logo" />
+
+                    <button className="flex items-center gap-2 rounded-full bg-gray-100 px-4 py-2 text-sm font-medium hover:bg-gray-200">
+                        <FontAwesomeIcon icon={faPlus} />
+                        <span className="hidden sm:inline">Create</span>
+                    </button>
+
+                    <button className="flex h-10 w-10 items-center justify-center rounded-full hover:bg-gray-900">
+                        <FontAwesomeIcon icon={faBell} className="text-lg" />
+                    </button>
+
+                    <button className="flex h-10 w-10 items-center justify-center rounded-full hover:bg-gray-900">
+                        <FontAwesomeIcon icon={faCircleUser} className="text-xl" />
+                    </button>
+
                 </div>
-                <div className="flex items-center gap-2 absolute left-1/2 transform -translate-x-1/2 ">
-                    <input className="  w-[500px] px-10   border-[0.1px] p-2  rounded-full" type="text" placeholder="Search..." />
-                    <button>Search</button>
-                </div>
-                <div className="absolute right-4 flex items-center gap-2">
-                    <button> <FontAwesomeIcon icon={faPlus} /> Create </button>
-                    <a href="" > <FontAwesomeIcon icon={faBell} /> </a>
-                    <a href="" > <FontAwesomeIcon icon={faCircleUser} /> </a>
-                </div>
-            </div>
+
+            </header>
         </ >
     )
 }
