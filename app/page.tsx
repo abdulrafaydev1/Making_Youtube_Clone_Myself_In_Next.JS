3039e54f0aa071b98b3ca0089ca91657602e9c3b
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-09-05 06:40:49 +0500

    making youtube clone myself

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+ 
+
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Next.js!</h1>
+      <Image src="/next.svg" alt="Next.js logo" width={100} height={100} />
+    </>
+  );
+}
